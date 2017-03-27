bb0b473771e42c77b353fbaed59ada05ba4c251f
Ibad Qureshi <ibrehman1995@gmail.com>
2017-03-27 16:43:05 +0500

    moving towards homeactivity

--- app/src/main/java/com/example/excaliburcreations/customerapp/SignupActivity.java
+++ app/src/main/java/com/example/excaliburcreations/customerapp/SignupActivity.java
@@ -1,13 +1,25 @@
 package com.example.excaliburcreations.customerapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
+    Button BtnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        BtnSignup = (Button) findViewById(R.id.BtnSignUp);
+        BtnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
