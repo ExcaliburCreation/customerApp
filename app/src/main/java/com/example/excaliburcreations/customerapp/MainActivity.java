e11bcb8c1659acc1dd4419bbc12cb3fb34e9ac12
Ibad Qureshi <ibrehman1995@gmail.com>
2017-03-27 15:45:00 +0500

    adding splash screen

--- app/src/main/java/com/example/excaliburcreations/customerapp/MainActivity.java
+++ app/src/main/java/com/example/excaliburcreations/customerapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.excaliburcreations.customerapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                    startActivity(intent);
+
+                    MainActivity.this.finish();
+                }
+            }, 3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
