f8588a70b62b1d8d642cfa7f9903731572ecba73
Ibad Qureshi <ibrehman1995@gmail.com>
2017-03-29 16:08:22 +0500

    completing with classUserInfo and adding json firebase

---
+++ app/src/main/java/com/example/excaliburcreations/customerapp/ClassUserInfo.java
@@ -0,0 +1,81 @@
+package com.example.excaliburcreations.customerapp;
+
+/**
+ * Created by dell pc on 29-Mar-17.
+ */
+
+public class ClassUserInfo {
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    String address;
+    String area;
+    String city;
+
+    public ClassUserInfo(String firstName, String lastName, String email,String password,String address, String area, String city){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.area = area;
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
